ed7a290b36ca58e0a8d8f9a1b51699198bbbc15a
Tariq <tariquake@hotmail.com>
2021-09-10 20:30:46 +0200

    added texture_manager

--- wave/include/wave.h
+++ wave/include/wave.h
@@ -1,9 +1,10 @@
 #pragma once
 
 #include "pch/wavepch.h"
-#include "input/input.h"
+#include "events/input.h"
 #include "context/imgui_context.h"
-#include "asset/shader/shader.h"
 #include "window/window.h"
 #include "buffer/buffer.h"
-#include "asset/texture/texture.h"
+#include "assets/shader/shader.h"
+#include "assets/texture/texture.h"
+#include "assets/texture/texture_manager.h"
