b8ac1973b1367f241aba3449e1a64d27f6704502
Tariq <tariquake@hotmail.com>
2021-09-10 16:37:44 +0200

    Files restructured, CMakeLists amended

--- cmake/set_precompiled_header.cmake
+++ cmake/set_precompiled_header.cmake
@@ -1,14 +1,14 @@
 # Set PCH for VS project
-function(set_target_precompiled_header target headerFile sourceFile sourcePath)
+function(set_target_precompiled_header target header_file source_path)
     if(MSVC)
         message(STATUS "Setting precompiled headers for: '${target}'")
         set_target_properties(${target} 
             PROPERTIES 
-                COMPILE_OPTIONS "/Yu${headerFile}"
+                COMPILE_OPTIONS "/Yu${${header_file}}"
         )
-        set_source_files_properties(${${sourcePath}}
+        set_source_files_properties(${${source_path}}
             PROPERTIES 
-                COMPILE_OPTIONS "/Yc${headerFile}"
+                COMPILE_OPTIONS "/Yc${${header_file}}"
         )
     endif(MSVC)
 endfunction(set_target_precompiled_header)
--- wave/include/wave.h
+++ wave/include/wave.h
@@ -1,9 +1,9 @@
 #pragma once
 
-#include "wavepch.h"
-#include "input.h"
-#include "imgui_context.h"
-#include "shader.h"
-#include "window.h"
-#include "opengl_buffers.h"
-#include "opengl_texture.h"
+#include "pch/wavepch.h"
+#include "input/input.h"
+#include "context/imgui_context.h"
+#include "asset/shader/shader.h"
+#include "window/window.h"
+#include "buffer/buffer.h"
+#include "asset/texture/texture.h"
