c45398e2b1b3cca3f5d53770234e08cc804e0f91
Tariq <tariquake@hotmail.com>
2021-09-11 22:13:51 +0200

    changed coding style

--- wave/include/wave.h
+++ wave/include/wave.h
@@ -4,7 +4,7 @@
 #include "events/input.h"
 #include "context/imgui_context.h"
 #include "window/window.h"
-#include "buffer/buffer.h"
+#include "buffer/vertex_buffer.h"
 #include "assets/shader/shader.h"
 #include "assets/texture/texture.h"
 #include "assets/texture/texture_manager.h"
